9a1f4e65a099aa18b81ca95dd2b3231032da92ea
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-12 17:59:10 +0200

    articles file created

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,71 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Article from '../components/Article'
 
 const Blog = () => {
+  const [content, setContent] = useState('')
+  const [author, setAuthor] = useState('')
+  const [errorMessage, setErrorMessage] = useState(false)
+  const [blogData, setBlogData] = useState([])
+
+  const getData = () => {
+    axios
+    .get('http://localhost:3004/articles')
+    .then((res) => setBlogData(res.data))
+  }
+
+  useEffect(() => getData(), []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (content.length < 140) {
+      setErrorMessage(true)
+    } else {
+      axios.post('http://localhost:3004/articles', {
+        author,
+        content,
+        date: Date.now(),
+      })
+      setErrorMessage(false);
+      setAuthor("")
+      setContent("")
+      getData()
+    }
+  }
+
   return (
     <div className="blog-container">
       <h1>Blog</h1>
-      <form>
-        <input type="text"  placeholder='Name'/>
-        <textarea placeholder='Message'></textarea>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <textarea
+          style={{
+            border: errorMessage ? '1px solid red' : '1px solid #61dafb',
+          }}
+          placeholder="Message"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        {errorMessage && (
+          <ul style={{ color: 'red' }}>
+            Veuillez écrire un minimun de 140 caractères
+          </ul>
+        )}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   )
 }
