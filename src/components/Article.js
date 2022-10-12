7c15355b68157b0e9688bb7c097b193363996fb2
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-12 18:28:57 +0200

    edit article created now we can edit and updating

--- src/components/Article.js
+++ src/components/Article.js
@@ -18,33 +18,36 @@ const Article = ({ article }) => {
   }
 
   const handleEdit = () => {
-    setEditing(true)
     console.log(editing)
-  }
-
-  const handleUpdate = () => {
-    axios.put('http://localhost:3004/articles/' + article.id, {
-      editContent,
-      updated:Date.now()
+    const data = {
+      author: article.author,
+      content: editContent ? editContent : article.content,
+      date: article.date,
+      updatedDate: Date.now(),
+    }
+    axios.put('http://localhost:3004/articles/' + article.id, data).then(() => {
+      setEditing(false)
     })
   }
+
   return (
-    <div className="article">
+    <div className="article" style={{background: editing ? "#f3feff" : "withe"}}>
       <div className="card-header">
         <h3>{article.author}</h3>
         <em>Posté le: {dateFormater(article.date)}</em>
       </div>
       {editing ? (
         <textarea
-          defaultValue={article.content}
+        autoFocus
+          defaultValue={editContent ? editContent : article.content}
           onChange={(e) => setEditContent(e.target.value)}
         ></textarea>
       ) : (
-        <p>{article.content}</p>
+        <p>{editContent ? editContent : article.content}</p>
       )}
       <div className="btn-container">
         {editing ? (
-          <button onClick={() => handleUpdate()}>Valider</button>
+          <button onClick={() => handleEdit()}>Valider</button>
         ) : (
           <button onClick={() => setEditing(true)}>Edit</button>
         )}
